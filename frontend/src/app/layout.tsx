ff16c8504ba7f826e5247aeac5206f8f8a075b69
Purav2003 <puravshah20@gnu.ac.in>
2024-04-05 20:52:24 +0530

    Updated

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import logo from "../assets/images/logo.png"
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <title>Cloud Enabled Attendance System</title>
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+      <NextTopLoader   showSpinner={false}/>
+        {children}
       </body>
     </html>
   )
